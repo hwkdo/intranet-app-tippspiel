2fb8054b48f58611d97ed557ff8097393af79bbe
aleex1848 <halycon01@gmail.com>
2026-08-28 12:14:19 +0000

    fix UserSettings

--- routes/web.php
+++ routes/web.php
@@ -4,24 +4,35 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::middleware(['web', 'auth', 'can:see-app-tippspiel'])->group(function () {
     Route::livewire('apps/tippspiel', 'intranet-app-tippspiel::apps.tippspiel.dashboard')->name('apps.tippspiel.index');
+
     Route::livewire('apps/tippspiel/ewige-rangliste', 'intranet-app-tippspiel::apps.tippspiel.ewige-rangliste')->name('apps.tippspiel.ewige-rangliste');
+
     Route::livewire('apps/tippspiel/archiv/{season}', 'intranet-app-tippspiel::apps.tippspiel.archiv')->name('apps.tippspiel.archiv');
+
     Route::livewire('apps/tippspiel/tippen/{season}', 'intranet-app-tippspiel::apps.tippspiel.tippen')->name('apps.tippspiel.tippen');
+
     Route::livewire('apps/tippspiel/rangliste/{season}', 'intranet-app-tippspiel::apps.tippspiel.rangliste')->name('apps.tippspiel.rangliste');
+
     Route::livewire('apps/tippspiel/ergebnisse/{season}', 'intranet-app-tippspiel::apps.tippspiel.ergebnisse')->name('apps.tippspiel.ergebnisse');
+
     Route::livewire('apps/tippspiel/auswertungen/{season}', 'intranet-app-tippspiel::apps.tippspiel.auswertungen')->name('apps.tippspiel.auswertungen');
+
     Route::livewire('apps/tippspiel/auswertung/{season}/{roundSlug}', 'intranet-app-tippspiel::apps.tippspiel.runden-auswertung')
         ->where('roundSlug', 'md-\d+|stage-.+')
         ->name('apps.tippspiel.auswertung');
 
+
     Route::livewire('apps/tippspiel/admin', \Hwkdo\IntranetAppTippspiel\Livewire\Apps\Tippspiel\Admin\Index::class)
         ->middleware('can:manage-app-tippspiel')
         ->name('apps.tippspiel.admin.index');
+
     Route::livewire('apps/tippspiel/admin/saisons', 'intranet-app-tippspiel::apps.tippspiel.admin.saisons')
         ->middleware('can:manage-app-tippspiel')
         ->name('apps.tippspiel.admin.saisons');
+
     Route::redirect('apps/tippspiel/admin/einstellungen', '/apps/tippspiel/admin?activeTab=einstellungen')
         ->middleware('can:manage-app-tippspiel')
         ->name('apps.tippspiel.admin.einstellungen');
